fe1975bbb2281784898c1cfc2fa5ced7e3b9564d
Cristofer Casas <cristofer32513@gmail.com>
2019-02-13 22:12:15 -0600

    Se agrego la etiqueta de Estandar a la ventana.

--- src/PruebaCalculadora.java
+++ src/PruebaCalculadora.java
@@ -36,10 +36,42 @@ class Calculadora{
 	}
 }
 
+class VentanaCalculadora extends JFrame {
+	
+	JLabel lblEstandar, lblHistorial, lblPantalla;
+	JButton btnPorcentaje, btnRaiz, btnAlCuadrado, btnUnoEntreX, btnCE, btnC, btnBorrar,
+		btnDividir, btnMultiplicar, btnRestar, btnSumar, btnIgual, btnPunto, btnMasMenos,
+		btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+	
+	
+	public VentanaCalculadora() {
+		//Configuracon de la ventana
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setTitle("Calculadora");
+		setVisible(true);
+		
+		//Componentes
+		lblEstandar=new JLabel("Estándar");
+			lblEstandar.setFont(new Font("Times New Roman", 0, 30));
+			lblEstandar.setBounds(20, 15, 120, 20);
+		add(lblEstandar);
+	}
+	
+	
+}
 
 public class PruebaCalculadora {
 
 	public static void main(String[] args) {
-		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new VentanaCalculadora();
+			}
+		});
 	}
 }
