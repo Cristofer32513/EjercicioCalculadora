2b39518eb84eb6443e49c3695f892a823f660744
Cristofer Casas <cristofer32513@gmail.com>
2019-02-13 21:50:55 -0600

    Se creo la clase Calculadora y se agregaron algunos metodos sin codigo.

--- src/PruebaCalculadora.java
+++ src/PruebaCalculadora.java
@@ -1,3 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Calculadora{
+	
+	public double porcentaje(){
+		return 0.0;
+	}
+	
+	public double raizCuadrada(){
+		return 0.0;
+	}
+	
+	public double alCuadrado(){
+		return 0.0;
+	}
+	
+	public double unoEntreX(){
+		return 0.0;
+	}
+	
+	public double suma(){
+		return 0.0;
+	}
+	
+	public double resta(){
+		return 0.0;
+	}
+	
+	public double multiplicacion(){
+		return 0.0;
+	}
+	
+	public double division(){
+		return 0.0;
+	}
+}
+
 
 public class PruebaCalculadora {
 
